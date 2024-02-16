ecfddb64263078d1a49e6a86fe1dbd1662b47c8f
cutekibry <cutekibry@gmail.com>
2024-02-16 13:49:31 +0800

    feat(styled_button): 添加 disabled 和更改颜色功能
    添加了 button_disabled.png 以及根据主题更改配色功能。

--- objects/styled_button/styled_button.gd
+++ objects/styled_button/styled_button.gd
@@ -5,3 +5,11 @@ class_name StyledButton
 
 func _on_pressed():
 	$SFXButtonDown.play()
+
+
+func _ready():
+	for box_theme in ["normal", "hover", "presssed", "disabled", "focus"]:
+		var stylebox := get_theme_stylebox(box_theme)
+		if stylebox is StyleBoxTexture:
+			stylebox.texture = ImageLib.replace_palette_colors_in_image(stylebox.texture)
+			add_theme_stylebox_override(box_theme, stylebox)
