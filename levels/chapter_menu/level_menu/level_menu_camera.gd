b02634880b0e317e531001b48ff6159c77fc3ee4
cutekibry <cutekibry@gmail.com>
2024-02-18 19:16:36 +0800

    feat(level_menu)!: 修改场景结构，添加选择章节功能
    LevelMenu 的场景结构发生变化：
    
    - LevelMenu
      - LevelMenuCamera # 相机
        - SmoothMovement
      - LevelButtons # 选关按钮
      - UI
        - PreviousLevelButton
        - NextLevelButton
        - Title
    
    初始化时，会生成所有的选关按钮。
    
    按屏幕左右的按钮（PreviousLevelButton 或 NextLevelButton）可以移动
    相机，从而实现切换章节的功能。
    
    BREAKING CHANGE: LevelMenu 的场景结构发生变化。

---
+++ levels/chapter_menu/level_menu/level_menu_camera.gd
@@ -0,0 +1,22 @@
+extends Camera2D
+
+
+const WIDTH := 1920 / 4
+const HEIGHT := 1080 / 4
+const MOVE_TIME := 0.3
+
+
+func _process(_delta):
+	self.position = $SmoothMovement.get_position()
+	# print($SmoothMovement.get_position())
+
+func init_position(pos: Vector2) -> void:
+	$SmoothMovement.set_position(pos)
+
+
+func _on_next_chapter_button_pressed():
+	$SmoothMovement.move_to($SmoothMovement.get_position() + Vector2(WIDTH, 0), MOVE_TIME)
+
+
+func _on_previous_chapter_button_pressed():
+	$SmoothMovement.move_to($SmoothMovement.get_position() - Vector2(WIDTH, 0), MOVE_TIME)
