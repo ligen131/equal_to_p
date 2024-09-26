e4fa94537708163eec743478ad6fc3dc94facc76
cutekibry <cutekibry@gmail.com>
2024-09-27 02:40:27 +0800

    feat: 添加 'M 并重构 word.tscn 精灵表导出功能
    - 添加了 'M 字符。
    - 重构了 word.tscn 精灵表导出。现在将 word_sprite_sheet.ase 导出成一行
      word_sprite_sheet.png 后，运行 export.py 即可完成精灵图导出。

--- objects/word/word.gd
+++ objects/word/word.gd
@@ -2,23 +2,22 @@
 class_name Word extends AnimatedSprite2D
 
 
-
-
 ## 显示的字符下标。
 ## [br][br]
 ## 当前显示的字符应为 [code]LETTER_NAME[text_id][/code]。
-@export var text_id := 0 
+@export var text_id := 0
 
 @export var color := Color.BLACK ## 当前字符颜色。
 
 
-const STEP := 30 ## 在字符动画的资源文件夹（[code]res://objects/word/sprites[/code]）中，同一个字符相邻帧之间的文件编号差。
-const LETTER_NAME = [" ", "=", "P", "b", "D", 
-"I1", "I2", "I3", "I4", "I5", 
+const STEP := 36 ## 在字符动画的资源文件夹（[code]res://objects/word/sprites[/code]）中，同一个字符相邻帧之间的文件编号差。
+const LETTER_NAME: Array[String] = [" ", "=", "P", "b", "D",
+"I1", "I2", "I3", "I4", "I5",
 "R", "Q", "d", "q", "(",
 "*", "+", "<", ">", ")",
 "0", "1", "2", "3", "4",
-"5", "6", "7", "8", "9"] ## 可取的字符列表。
+"5", "6", "7", "8", "9",
+"M", "", "", "", ""] ## 可取的字符列表。
 
 
 ## 获取字符 [param letter] 在 [member LETTER_NAME] 中对应的下标。
@@ -41,10 +40,9 @@ func set_text_id(value: int) -> void:
 
 ## 根据当前的 [member text_id] 更新动画。
 func update_animation() -> void:
-	ImageLib.update_animation(self, text_id + 1, 3, STEP, "res://objects/word/sprites/sprite%d.png")
+	ImageLib.update_animation(self, text_id + 1, 3, STEP, "res://objects/word/assets/exports/sprite%d.png")
 	
 	
-
 ## 设置字符为 [param text]。
 ## [br][br]
 ## [b]注意[/b]：若 [param text] 为 [code]_[/code] 或 [code].[/code]，则会设置为 [code] [/code]。但不推荐使用该做法。
@@ -78,7 +76,6 @@ func set_victory(v: bool) -> void:
 		set_color(ImageLib.get_palette_color_by_name("golden"))
 	else:
 		set_color(ImageLib.get_palette_color_by_name("black"))
-
 
 
 func _ready():
